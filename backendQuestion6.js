89153b12156d67ccc21af1408c942eb8494ad913
Schwyn Francis <schwynf@gmail.com>
2021-05-21 08:30:52 -0700

    removed console.log

--- backendQuestion6.js
+++ backendQuestion6.js
@@ -6,18 +6,11 @@ const compress =  (string) => {
     for (let i = 0; i < stringArray.length; i++) {
         let count = 1;
         let char = stringArray[i];
-        if(char.search(/[a-z]/) > -1){
             while (i < stringArray.length - 1 && stringArray[i] == stringArray[i + 1]) {
                 count++;
                 i++;
             }
-            compressedString += char + count;
-            
-        }else{
-
-            compressedString += char;
-           
-        }
+            compressedString += char + count;         
     }
     if(compressedString.length < string.length){
         return compressedString;
@@ -27,21 +20,18 @@ const compress =  (string) => {
 }
 
 // function will decompress string
+
 const decompress =  (string) => {
     if(string.match(/[0-9]/g) == null){
-        return string
+        return string;
     }
     let decompressedString = "";
     for (let i = 0; i < string.length;) {
         let char = string[i];
         let count = 0;
         i++;
-        if(char.search(/[a-z]/) === -1){
-            decompressedString += char;
-        }
         while (i < string.length && parseInt(string[i]) >= 1 && parseInt(string[i]) <= 9) {
-            console.log(parseInt(string[i]))
-            count =  parseInt(count * 10) + parseInt(string[i]);
+            count =  count * 10 + parseInt(string[i]);
             i++;
         }
         while (count > 0) {
@@ -51,5 +41,3 @@ const decompress =  (string) => {
     }
     return decompressedString;
 }
-
-
